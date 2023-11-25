47146c10c1873e2a8c6f7a42d7147fef76aae9c6
Terina <ktrn.147@mail.ru>
2023-11-25 12:37:12 +0300

    add controllers (all)

---
+++ E-Journal.API/Controllers/Entities/LessonController.cs
@@ -0,0 +1,15 @@
+﻿using E_Journal.API.Controllers.Abstract;
+using E_Journal.Domain.Model.Entities;
+using E_Journal.Infrastructure;
+using Microsoft.AspNetCore.Mvc;
+
+namespace E_Journal.API.Controllers.Entities
+{
+
+    [ApiController]
+    [Route("[controller]")]
+    public class LessonController : CRUDController<Lesson>
+    {
+        public LessonController(DBContext dbContext) : base(dbContext) { }
+    }
+}
